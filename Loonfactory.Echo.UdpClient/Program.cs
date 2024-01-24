cd7f20483d4872541e934c8c3a3737b9d5e5d6f9
loonfactory <thsdnjstjq1@naver.com>
2024-01-24 20:53:43 +0900

    초기 커밋.

---
+++ Loonfactory.Echo.UdpClient/Program.cs
@@ -0,0 +1,29 @@
+﻿using System.Net.Sockets;
+using System.Net;
+using System.Text;
+
+UdpClient udpClient = new();
+Console.Write("Please enter the UDP server address and port (ex: 192.168.0.1:1234): ");
+var input = Console.ReadLine();
+
+IPEndPoint? serverEndPoint;
+while (!IPEndPoint.TryParse(input!, out serverEndPoint))
+{
+    Console.WriteLine("Invalid input. Please try again.");
+    input = Console.ReadLine();
+}
+
+udpClient.Connect(serverEndPoint);
+
+while (true)
+{
+    input = Console.ReadLine();
+    var buffer = Encoding.UTF8.GetBytes(input!);
+    udpClient.Send(buffer, buffer.Length);
+
+    Console.WriteLine("Send: " + input);
+
+    buffer = udpClient.Receive(ref serverEndPoint);
+
+    Console.WriteLine("Received: " + BitConverter.ToString(buffer));
+}
---
+++ Loonfactory.Echo.UdpServer/Program.cs
@@ -0,0 +1,31 @@
+﻿using System.Net.Sockets;
+using System.Net;
+
+const int Port = 29291;
+
+UdpClient udpServer = new(Port);
+Console.WriteLine("UDP Echo Server is running on port " + Port);
+
+try
+{
+    while (true)
+    {
+        // 클라이언트의 IP 엔드포인트를 저장할 변수
+        var clientEP = new IPEndPoint(IPAddress.Any, 0);
+
+        // 클라이언트로부터 메시지를 받음
+        byte[] receivedBytes = udpServer.Receive(ref clientEP);
+
+        // 받은 메시지를 클라이언트에게 다시 보냄
+        udpServer.Send(receivedBytes, receivedBytes.Length, clientEP);
+        Console.WriteLine("Received: " + BitConverter.ToString(receivedBytes) + " from " + clientEP.ToString());
+    }
+}
+catch (Exception ex)
+{
+    Console.WriteLine(ex.ToString());
+}
+finally
+{
+    udpServer.Close();
+}
