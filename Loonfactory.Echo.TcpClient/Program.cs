f63aedf6bbd345a4f715384114d8f0f69074789a
loonfactory <thsdnjstjq1@naver.com>
2024-01-26 11:31:17 +0900

    Echo Tcp

---
+++ Loonfactory.Echo.TcpClient/Program.cs
@@ -0,0 +1,30 @@
+﻿using System.Net.Sockets;
+using System.Net;
+using System.Text;
+
+Console.Write("Please enter the Tcp echo server address and port (ex: 192.168.0.1:1234): ");
+TcpClient tcpClient = new();
+var input = Console.ReadLine();
+
+IPEndPoint? serverEndPoint;
+while (!IPEndPoint.TryParse(input!, out serverEndPoint))
+{
+    Console.WriteLine("Invalid input. Please try again.");
+    input = Console.ReadLine();
+}
+
+tcpClient.Connect(serverEndPoint);
+using var stream = tcpClient.GetStream();
+
+while (true)
+{
+    Console.Write("Send: ");
+    input = Console.ReadLine();
+    var buffer = Encoding.UTF8.GetBytes(input!);
+
+    await stream.WriteAsync(buffer);
+
+    var count = await stream.ReadAsync(buffer.AsMemory());
+
+    Console.WriteLine("Received: " + Encoding.UTF8.GetString(buffer));
+}
---
+++ Loonfactory.Echo.TcpServer/Program.cs
@@ -0,0 +1,42 @@
+﻿using System.Net.Sockets;
+using System.Net;
+using System.Text;
+
+const int Port = 29290;
+
+var listener = new TcpListener(IPAddress.Any, Port);
+listener.Start();
+Console.WriteLine($"TCP Echo Server is running on port {Port}");
+
+while (true)
+{
+    var client = await listener.AcceptTcpClientAsync();
+    _ = Task.Run(() => HandleClient(client));
+}
+
+async Task HandleClient(TcpClient client)
+{
+    using var stream = client.GetStream();
+    var endPoint = (client.Client.RemoteEndPoint as IPEndPoint)!;
+    Console.WriteLine($"[{endPoint.Address}:{endPoint.Port}] Client connected.");
+    var buffer = new byte[1024];
+    int count;
+
+    try
+    {
+        while ((count = await stream.ReadAsync(buffer)) != 0)
+        {
+            Console.WriteLine($"[{endPoint.Address}:{endPoint.Port}] {BitConverter.ToString(buffer, 0, count)}");
+            await stream.WriteAsync(buffer.AsMemory(0, count));
+        }
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine($"[{endPoint.Address}:{endPoint.Port}] Client handling error: " + ex.Message);
+    }
+    finally
+    {
+        client.Close();
+        Console.WriteLine($"[{endPoint.Address}:{endPoint.Port}] Client disconnected.");
+    }
+}
